f439654b9c576ebc97150668e9148905799b6fb8
Abdallah Abdelnaser <abdallahabdelnaser3@gmail.com>
2025-10-28 19:40:07 +0300

    build trial

--- src/components/layout/navbar/NavBar.tsx
+++ src/components/layout/navbar/NavBar.tsx
@@ -68,7 +68,7 @@ export default function NavBar() {
             sx={{ cursor: "pointer" }}
             variant="titleSpecial"
           >
-            trial 
+            abo trika  
           </Typography>
         </div>
         <Box
