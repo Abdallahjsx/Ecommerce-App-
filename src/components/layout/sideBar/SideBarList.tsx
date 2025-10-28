5e3192bd9fc88446905a81230090560b65eaa68f
Abdallah Abdelnaser <abdallahabdelnaser3@gmail.com>
2025-10-28 20:19:38 +0300

    never mind

--- src/components/layout/sideBar/SideBarList.tsx
+++ src/components/layout/sideBar/SideBarList.tsx
@@ -26,118 +26,126 @@ export default function SideBarList({
   return (
     <Box
       sx={{
-        padding: "6px 20px 153px 12px",
-        display: ["flex", "flex", "none"],
-        flexDirection: "column",
+        display: ["block", "block", "none"],
         position: "fixed",
-        overflowY: "scroll",
-        scrollbarWidth: "none",
-        height: "110vh",
-        width: "290px",
+        left: `${shown ? "0px" : "-100vw"}`,
+        zIndex: -11,
         top: "70px",
         bottom: "-20px",
-        zIndex: -11,
-        left: `${shown ? "0px" : "-350px"}`,
+        backdropFilter: `${shown ? `brightness(0.8)` : `brightness(8)`}`,
         transition: "all 0.5s ease",
-        backgroundColor: t.tokens.backgroundColors.main,
+        width: "100vw",
       }}
     >
-      {!loggedIn ? (
-        <div
-          style={{
-            padding: "9px 0px",
-            marginBottom: "10px",
-
-            width: "100%",
-          }}
-        >
-          <Typography variant="link" fontSize={14} color="black">
-            Login for more personalized experience
-          </Typography>
-          <div
-            style={{
-              padding: "0px",
-              display: "flex",
-              flexDirection: "column",
-              gap: 9,
-              marginTop: 16,
-            }}
-          >
-            <Button
-              sx={{ bgcolor: t.palette.primary.main, borderRadius: "8px" }}
-              onClick={() => {
-                router.push("/login");
-              }}
-            >
-              <Typography variant="link" fontSize={14} color="white">
-                Login
-              </Typography>
-            </Button>
-            <Button
-              sx={{ bgcolor: "white", borderRadius: "8px" }}
-              onClick={() => {
-                router.push("/register");
-              }}
-            >
-              <Typography
-                variant="link"
-                component={"a"}
-                href="/register"
-                fontSize={14}
-                color="primary"
-              >
-                Register
-              </Typography>
-            </Button>
-          </div>
-        </div>
-      ) : (
-        <></>
-      )}
-      <section
-        style={{
-          padding: "10px 10px 10px 0px",
-          display: "flex",
+      <Box
+        sx={{
+          padding: "6px 20px 153px 12px",
+          display: ["flex", "flex", "none"],
           flexDirection: "column",
-          marginBottom: "10px",
+          overflowY: "scroll",
+          scrollbarWidth: "none",
+          height: "110vh",
+          width: "290px",
+          backgroundColor: t.tokens.backgroundColors.main,
         }}
       >
-        {optionsList.map((op: any, index: number) => (
+        {!loggedIn ? (
           <div
-            key={index}
             style={{
-              display: "flex",
-              gap: "12px",
-              padding: "20px 0px",
-              justifyContent: "flex-start",
-              // alignItems:"center"
+              padding: "9px 0px",
+              marginBottom: "10px",
+
+              width: "100%",
             }}
           >
-            <img src={op.icon} alt={op.name} />
-            <Typography variant="link" color="primary">
-              {op.name}
+            <Typography variant="link" fontSize={14} color="black">
+              Login for more personalized experience
             </Typography>
+            <div
+              style={{
+                padding: "0px",
+                display: "flex",
+                flexDirection: "column",
+                gap: 9,
+                marginTop: 16,
+              }}
+            >
+              <Button
+                sx={{ bgcolor: t.palette.primary.main, borderRadius: "8px" }}
+                onClick={() => {
+                  router.push("/login");
+                }}
+              >
+                <Typography variant="link" fontSize={14} color="white">
+                  Login
+                </Typography>
+              </Button>
+              <Button
+                sx={{ bgcolor: "white", borderRadius: "8px" }}
+                onClick={() => {
+                  router.push("/register");
+                }}
+              >
+                <Typography
+                  variant="link"
+                  component={"a"}
+                  href="/register"
+                  fontSize={14}
+                  color="primary"
+                >
+                  Register
+                </Typography>
+              </Button>
+            </div>
           </div>
-        ))}
-      </section>
-      <section>
-        <Typography
-          fontSize={16}
-          variant="titleSpecial"
-          color="priamry"
-          sx={{ marginBottom: "16px" }}
+        ) : (
+          <></>
+        )}
+        <section
+          style={{
+            padding: "10px 10px 10px 0px",
+            display: "flex",
+            flexDirection: "column",
+            marginBottom: "10px",
+          }}
         >
-          GET THE APP
-        </Typography>
-        <div style={{ display: "flex", gap: "8px" }}>
-          <Button style={{ padding: "0px" }}>
-            <img src={"/assets/images/button-appstore.png"} alt="" />
-          </Button>
-          <Button>
-            <img src={"/assets/images/button-playstore.png"} alt="" />
-          </Button>
-        </div>
-      </section>
+          {optionsList.map((op: any, index: number) => (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                gap: "12px",
+                padding: "20px 0px",
+                justifyContent: "flex-start",
+                // alignItems:"center"
+              }}
+            >
+              <img src={op.icon} alt={op.name} />
+              <Typography variant="link" color="primary">
+                {op.name}
+              </Typography>
+            </div>
+          ))}
+        </section>
+        <section>
+          <Typography
+            fontSize={16}
+            variant="titleSpecial"
+            color="priamry"
+            sx={{ marginBottom: "16px" }}
+          >
+            GET THE APP
+          </Typography>
+          <div style={{ display: "flex", gap: "8px" }}>
+            <Button style={{ padding: "0px" }}>
+              <img src={"/assets/images/button-appstore.png"} alt="" />
+            </Button>
+            <Button>
+              <img src={"/assets/images/button-playstore.png"} alt="" />
+            </Button>
+          </div>
+        </section>
+      </Box>
     </Box>
   );
 }
