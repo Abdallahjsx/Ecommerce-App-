f4a2cf47f0bf7d6b074988c18f6b1af19888b6cb
Abdallah Abdelnaser <abdallahabdelnaser3@gmail.com>
2026-04-19 21:25:30 +0200

    Merging Fixed Bugs

--- routes.tsx
+++ routes.tsx
@@ -5,7 +5,7 @@ const routes = [
   { title: "Orders", path: "/orders", private: true },
   { title: "Contact Us", path: "/support/contact-us", private: false },
   { title: "FAQS", path: "/faqs", private: false },
-  { title: "Profile", path: "/profile", private: true },
+  // { title: "Profile", path: "/profile", private: true },
   { title: "Wishlist", path: "/wishlist", private: true },
   { title: "Cart", path: "/cart", private: true },
 ];
